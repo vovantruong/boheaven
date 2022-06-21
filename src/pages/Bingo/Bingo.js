c686ec247f25ba00ab5129162af59b86b3abc621
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2022-06-21 09:36:01 +0700

    commit setup routes

---
+++ src/pages/Bingo/Bingo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bingo = () => {
+  return (
+    <div>Bingo</div>
+  )
+}
+
+export default Bingo
---
+++ src/pages/RealPerson/RealPerson.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RealPerson = () => {
+  return (
+    <div>RealPerson</div>
+  )
+}
+
+export default RealPerson
