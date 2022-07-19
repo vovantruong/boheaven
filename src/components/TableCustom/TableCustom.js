8bf412f8050014731858214a1de5519204792143
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2022-07-19 17:29:59 +0700

    commit

--- src/components/TableCustom/TableCustom.js
+++ src/components/TableCustom/TableCustom.js
@@ -5,33 +5,52 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 const TableCustom = ({
-  dataSource,
-  columns,
-  className,
-  headClassName,
-  bodyClassName,
+  dataSource = [],
+  columns = [],
+  className = "",
+  headClassName = "",
+  bodyClassName = "",
+  background = true,
+  panigation = false,
   children,
   colSpanHead,
 }) => {
   return (
-    <table className={cx("table-custom", className)}>
-      <thead className={cx(headClassName)}>
-        <tr>
-          {columns.map((item, i) => (
-            <th key={i} colSpan={colSpanHead}>{item?.name ? item.name : item}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody className={cx(bodyClassName)}>
-        {dataSource.map((item, i) => (
-          <tr key={i}>
-            {item.data.map((child, index) => (
-              <td key={index}>{child.name}</td>
+    <div className={cx("table-custom", className, !background && "non-bg")}>
+      <table>
+        <thead className={cx(headClassName)}>
+          <tr>
+            {columns.map((item, i) => (
+              <th key={i} colSpan={colSpanHead}>
+                {item?.name ? item.name : item}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className={cx(bodyClassName)}>
+          {dataSource.map((item, i) => (
+            <tr key={i}>
+              {item.data.map((child, index) => (
+                <td key={index}>{child.name}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {panigation && 
+        <nav className={cx("panigation")}>
+          <ul className={cx('list-btn')}>
+              <li className={cx('disable')}>{'<'}</li>
+              <li className={cx('active')}>1</li>
+              <li>2</li>
+              <li>...</li>
+              <li>9</li>
+              <li>10</li>
+              <li>{'>'}</li>
+          </ul>
+        </nav>
+      }
+    </div>
   );
 };
 
---
+++ src/constants/mocks/DataInquiryCompliance/dataInquiry.js
@@ -0,0 +1,158 @@
+export const dataInquiry = [
+    {
+        id: 1,
+        data: [
+            {
+                key: 'type',
+                name: '儲值'
+            },
+            {
+                key: 'amount',
+                name: '1000.00'
+            },
+            {
+                key: 'running-water',
+                name: '1000.00'
+            },
+            {
+                key: 'time',
+                name: '2022-03-30 15:20:51'
+            },
+            {
+                key: 'transaction',
+                name: 'DC2220646HC5'
+            },
+
+        ]
+    },
+    {
+        id: 2,
+        data: [
+            {
+                key: 'type',
+                name: '儲值'
+            },
+            {
+                key: 'amount',
+                name: '200.00'
+            },
+            {
+                key: 'running-water',
+                name: '1600.00'
+            },
+            {
+                key: 'time',
+                name: '2022-03-30 15:20:51'
+            },
+            {
+                key: 'transaction',
+                name: 'DC2220646HC5'
+            },
+
+        ]
+    },
+    {
+        id: 3,
+        data: [
+            {
+                key: 'type',
+                name: '儲值'
+            },
+            {
+                key: 'amount',
+                name: '1000.00'
+            },
+            {
+                key: 'running-water',
+                name: '1000.00'
+            },
+            {
+                key: 'time',
+                name: '2022-03-30 15:20:51'
+            },
+            {
+                key: 'transaction',
+                name: 'DC2220646HC5'
+            },
+
+        ]
+    },
+    {
+        id: 4,
+        data: [
+            {
+                key: 'type',
+                name: '儲值'
+            },
+            {
+                key: 'amount',
+                name: '200.00'
+            },
+            {
+                key: 'running-water',
+                name: '1600.00'
+            },
+            {
+                key: 'time',
+                name: '2022-03-30 15:20:51'
+            },
+            {
+                key: 'transaction',
+                name: 'DC2220646HC5'
+            },
+
+        ]
+    },
+    {
+        id: 5,
+        data: [
+            {
+                key: 'type',
+                name: '儲值'
+            },
+            {
+                key: 'amount',
+                name: '1000.00'
+            },
+            {
+                key: 'running-water',
+                name: '1000.00'
+            },
+            {
+                key: 'time',
+                name: '2022-03-30 15:20:51'
+            },
+            {
+                key: 'transaction',
+                name: 'DC2220646HC5'
+            },
+
+        ]
+    },
+    {
+        id: 6,
+        data: [
+            {
+                key: 'type',
+                name: '儲值'
+            },
+            {
+                key: 'amount',
+                name: '200.00'
+            },
+            {
+                key: 'running-water',
+                name: '1600.00'
+            },
+            {
+                key: 'time',
+                name: '2022-03-30 15:20:51'
+            },
+            {
+                key: 'transaction',
+                name: 'DC2220646HC5'
+            },
+
+        ]
+    }
+]
