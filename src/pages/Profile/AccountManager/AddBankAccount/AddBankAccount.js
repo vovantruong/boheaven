25c1b812dd68b943058ac12497ff2f7a6079a94c
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2022-07-20 16:42:04 +0700

    commit custom routes children mutiple route not navbar

--- src/pages/Profile/AccountManager/AddBankAccount/AddBankAccount.js
+++ src/pages/Profile/AccountManager/AddBankAccount/AddBankAccount.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import styles from './AddBankAccount.module.scss'
+import {useNavigate, useOutletContext} from 'react-router-dom'
 
 const cx = classNames.bind(styles)
 
 const AddBankAccount = () => {
+  const navigate = useNavigate();
   return(
     <div className={cx('')}>
-        AddBankAccount
+
     </div>
   )
 }
--- src/pages/Profile/AccountManager/AddCreditCard/AddCreditCard.js
+++ src/pages/Profile/AccountManager/AddCreditCard/AddCreditCard.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import styles from './AddCreditCard.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 const cx = classNames.bind(styles)
 
 const AddCreditCard = () => {
+  const navigate = useNavigate();
   return(
     <div className={cx('')}>
-        AddCreditCard
+        <button onClick={() => navigate(-1)}>Back</button>
+        Add Credit Card
     </div>
   )
 }
