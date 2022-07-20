d775b264789556d5dccd7f27e7ddbf26a726f132
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2022-07-20 11:12:49 +0700

    commit

---
+++ src/pages/Profile/AccountManager/AddCreditCard/AddCreditCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classNames from 'classnames/bind'
+import styles from './AddCreditCard.module.scss'
+
+const cx = classNames.bind(styles)
+
+const AddCreditCard = () => {
+  return(
+    <div className={cx('')}>
+        AddCreditCard
+    </div>
+  )
+}
+export default AddCreditCard
