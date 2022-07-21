e808dfc75a101183c15eb9475104e93f4c6b0346
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2022-07-21 14:46:37 +0700

    commit compele UI add bank account & add credit card

--- src/pages/Profile/AccountManager/AddBankAccount/AddBankAccount.js
+++ src/pages/Profile/AccountManager/AddBankAccount/AddBankAccount.js
@@ -1,15 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import classNames from "classnames/bind";
 import styles from "./AddBankAccount.module.scss";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { BsChevronLeft } from "react-icons/bs";
+import ImgExample from "../../../../assets/images/pages/profile/accountManager/example-bank-account.png";
+import Select from "react-select";
 import InputText from "~/components/InputText/InputText";
 
 const cx = classNames.bind(styles);
 
+const options = [
+  { value: "瑞士银行", label: "瑞士银行" },
+  { value: "清华银行", label: "清华银行" },
+  { value: "北京银行", label: "北京银行" },
+];
+
+const customStyleSelect = {
+  control: (provided, state) => ({
+    // ...provided,
+    display: 'flex',
+    height: '3.4rem',
+    alignItems: 'center',
+    background: "transparent",
+    border: "1px solid #ABABAB",
+    cursor: "pointer",
+  }),
+  placeholder: (provided, state) => ({
+    ...provided,
+    color: "#cbcdd0",
+  }),
+  indicatorSeparator: () => ({}),
+};
+
 const AddBankAccount = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
+  const [bankCode, setBankCode] = useState(null);
+  const [bankAccount, setBankAccount] = useState("");
+
+  const fileRef = useRef();
+
+  const changeFile = (e) => {
+    // console.log(e);
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -21,15 +54,82 @@ const AddBankAccount = () => {
         <BsChevronLeft />
         <span>新增銀行帳戶</span>
       </div>
-      <div className={cx("upload-data")}>
+      <div className={cx("form-wrapper")}>
         <form onSubmit={onSubmit} autoComplete="off">
-          <InputText
-            name="username"
-            value={username}
-            wrapClassName={cx('username')}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="请输入您的账户名称"
-          />
+          <div className={cx("form-item")}>
+            <label className={cx('lb-name')} htmlFor="f-user">帳戶人姓名:</label>
+            <div className={cx("input")}>
+              <input
+                id="f-user"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="请输入您的账户名称"
+              />
+            </div>
+          </div>
+          <div className={cx('form-item')}>
+            <label className={cx('lb-name')}></label>
+            <span className={cx('notes')}>為了您的資金能夠迅速到帳，請確保填寫的姓名與銀行卡的開戶姓名一致</span>
+          </div>
+          <div className={cx("form-item")}>
+            <label className={cx('lb-name')}>銀行代碼與名稱:</label>
+            <Select
+              styles={customStyleSelect}
+              className={cx("bank-code")}
+              value={bankCode}
+              onChange={(e) => setBankCode(e)}
+              options={options}
+              placeholder="請選擇銀行"
+            />
+          </div>
+          <div className={cx("form-item")}>
+            <label className={cx('lb-name')} htmlFor="f-bank-account">銀行帳號:</label>
+            <div className={cx("input")}>
+              <input
+                id="f-bank-account"
+                value={bankAccount}
+                onChange={(e) => setBankAccount(e.target.value)}
+                placeholder="請輸入帳號"
+                type="number"
+              />
+            </div>
+          </div>
+          <div className={cx('form-item')}>
+            <label className={cx('lb-name')}></label>
+            <span className={cx('notes')}>請認真檢查儀行帳號，帳號錯誤資金無法到帳</span>
+          </div>
+          <div className={cx("form-item")}>
+            <div className={cx("file")}>
+              <label>
+                <span>
+                  點擊此處上傳驗證資料 <br /> 身分證+健保卡(或第二有效證件)+銀行存摺
+                  合拍(示意圖)<br /> *最多上傳40筆，上傳檔案大小請在5MB以下
+                </span>
+                <input
+                  ref={fileRef}
+                  onChange={changeFile}
+                  type="file"
+                  multiple
+                  accept=".jpg, .png, .jpeg, .gif"
+                />
+              </label>
+            </div>
+          </div>
+          <div className={cx("form-item")}>
+            <button className={cx("btn-upload")}>上傳</button>
+          </div>
+          <div className={cx("form-item")}>
+            <div className={cx("example")}>
+              <p>範例:</p>
+              <div className={cx("img-example")}>
+                <img src={ImgExample} />
+              </div>
+            </div>
+          </div>
+          <div className={cx("form-item", "group-btn")}>
+            <button className={cx("btn-finish")}>完成</button>
+            <button className={cx("btn-cancel")}>取消</button>
+          </div>
         </form>
       </div>
     </div>
--- src/pages/Profile/AccountManager/AddCreditCard/AddCreditCard.js
+++ src/pages/Profile/AccountManager/AddCreditCard/AddCreditCard.js
@@ -1,18 +1,107 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./AddCreditCard.module.scss";
 import { useNavigate } from "react-router-dom";
-import {BsChevronLeft} from 'react-icons/bs'
+import { BsChevronLeft } from "react-icons/bs";
+import ImgExample from "../../../../assets/images/pages/profile/accountManager/example-credit-card.png";
+import ImgUserExample from '../../../../assets/images/pages/profile/accountManager/example-use-card.png';
 
 const cx = classNames.bind(styles);
 
 const AddCreditCard = () => {
   const navigate = useNavigate();
+  const [cardName, setCardName] = useState();
+  const [cardNumber, setCardNumber] = useState("");
+
+  const fileRef = useRef();
+
+  const changeFile = (e) => {
+    // console.log(e);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={cx("credit-card", "bg-normal-linear")}>
       <div className={cx("go-back")} onClick={() => navigate(-1)}>
         <BsChevronLeft />
         <span>新增銀行帳戶</span>
+      </div>
+      <div className={cx("form-wrapper")}>
+        <form onSubmit={onSubmit} autoComplete="off">
+          <div className={cx("form-item")}>
+            <label className={cx("lb-name")} htmlFor="f-card-name">
+              持卡人姓名:
+            </label>
+            <div className={cx("input")}>
+              <input
+                id="f-card-name"
+                value={cardName}
+                onChange={(e) => setCardName(e.target.value)}
+                placeholder="请输入持卡人姓名"
+              />
+            </div>
+          </div>
+          <div className={cx("form-item")}>
+            <label className={cx("lb-name")}></label>
+            <span className={cx("notes")}>
+              為了您的資金能夠迅速到帳，請確保填寫的姓名與銀行卡的開戶姓名一致
+            </span>
+          </div>
+          <div className={cx("form-item")}>
+            <label className={cx("lb-name")} htmlFor="f-card-number">
+              信用卡卡號:
+            </label>
+            <div className={cx("input")}>
+              <input
+                id="f-card-number"
+                value={cardNumber}
+                onChange={(e) => setCardNumber(e.target.value)}
+                placeholder="請輸入卡號"
+              />
+            </div>
+          </div>
+          <div className={cx("form-item")}>
+            <div className={cx("file")}>
+              <label>
+                <span>
+                  點擊此處上傳信用卡(最多上傳20份、大小不超過5MB) <br />
+                  1.雙證件+信用卡合拍 <br />
+                  2.本人+信用卡合拍(手不能遮住名字跟卡號) <br />
+                  需顯示完整姓名卡號及日期，若會拍攝到三位安全碼，<br />
+                  請務必遮擋。
+                </span>
+                <input
+                  ref={fileRef}
+                  onChange={changeFile}
+                  type="file"
+                  multiple
+                  accept=".jpg, .png, .jpeg, .gif"
+                />
+              </label>
+            </div>
+          </div>
+          <div className={cx("form-item")}>
+            <button className={cx("btn-upload")}>上傳</button>
+          </div>
+          <div className={cx("form-item")}>
+            <div className={cx("example")}>
+              <p>範例:</p>
+              <div className={cx("img-example")}>
+                <img src={ImgExample} />
+              </div>
+              <div className={cx("img-user-example")}>
+                <img src={ImgUserExample} />
+              </div>
+            </div>
+          </div>
+          <div className={cx("form-item", "group-btn")}>
+            <button className={cx("btn-finish")}>完成</button>
+            <button className={cx("btn-cancel")}>取消</button>
+          </div>
+        </form>
       </div>
     </div>
   );
